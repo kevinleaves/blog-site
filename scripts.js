2c54bcd3dbd709e0713c98a12f5845901c5ce667
Kevin Le <iamkevinvle@gmail.com>
2022-08-14 15:32:17 -0700

    create project boilerplate

---
+++ scripts.js
@@ -0,0 +1,11 @@
+//dummy data
+
+// var blogPost = {
+//     'user': 'kevin',
+//     'title': 'sunday recap',
+//     'body': 'kevin watched basketball games and went grocery shopping',
+//     'date': '08/14/22', 
+// }
+
+
+
