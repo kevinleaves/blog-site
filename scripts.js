1eceaed26bbafef0b986b4a81a00d1553d8576db
Kevin Le <iamkevinvle@gmail.com>
2022-08-14 18:04:31 -0700

    populate dummy data array

--- scripts.js
+++ scripts.js
@@ -1,11 +1,40 @@
 //dummy data
+var dummyData = [
+  {
+  'user': 'kevin',
+  'title': 'sunday recap',
+  'body': 'hooped, went to whole foods, worked on app',
+  'date': '08/14/22',
+  },
+  {
+  'user': 'kenny',
+  'title': 'king of the court was sick',
+  'body': 'i crossed han up so bad lol.',
+  'date': '08/14/22',
+  },
+  {
+  'user': 'timmy',
+  'title': 'sunday is for chillin',
+  'body': 'went to the beach 2day it\'s whateva',
+  'date': '08/14/22',
+  },
+  {
+  'user': 'han',
+  'title': 'i can\'t shoot a jumpshot going right',
+  'body': 'i kept getting blocked today',
+  'date': '08/14/22',
+  },
+  {
+  'user': 'roger',
+  'title': 'good games today',
+  'body': 'pull-up jumpshot lookin NICE',
+  'date': '08/14/22',
+  },      
+]
 
-// var blogPost = {
-//     'user': 'kevin',
-//     'title': 'sunday recap',
-//     'body': 'kevin watched basketball games and went grocery shopping',
-//     'date': '08/14/22', 
-// }
-
-
-
+//dummy data array
+//for each obj
+  //create div for overall post
+  //create child divs for each obj key
+  //populate all divs with text content
+  //append post div to timeline div
